82e3f317ba3ea7856df2301174561d5f327af993
Ricky Hu <ricky9667@gmail.com>
2020-03-06 11:47:22 +0800

    Add seekbar, can change value and text when changing progress

--- app/src/main/java/com/example/kmtimer/MainActivity.java
+++ app/src/main/java/com/example/kmtimer/MainActivity.java
@@ -7,34 +7,42 @@ import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView timerTextView;
-    long totalTime = 5;
+    int totalTime = 60;
+
+    public String convertTimeText(int time) {
+        String minutes, seconds;
+
+        minutes = ((totalTime / 60 < 10)?"0":"") + (totalTime / 60);
+        seconds = ((totalTime % 60 < 10)?"0":"") + (totalTime % 60);
+
+        return minutes + ":" + seconds;
+    }
 
     public void startTimer(View view) {
 
-        new CountDownTimer(totalTime * 1000, 1000) {
+        final ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.title_image);
 
-            String minutes, seconds, timerText;
+        new CountDownTimer(totalTime * 1000, 1000) {
 
             public void onTick(long millisLeft) {
 
-                minutes = ((totalTime / 60 < 10)?"0":"") + (totalTime / 60);
-                seconds = ((totalTime % 60 < 10)?"0":"") + (totalTime % 60);
-
-                timerText = minutes + ":" + seconds;
-
-                timerTextView.setText(timerText);
+                timerTextView.setText(convertTimeText((int) millisLeft));
                 totalTime--;
             }
 
             public void onFinish() {
                 Toast.makeText(getApplicationContext(), "Time is out!", Toast.LENGTH_SHORT).show();
                 timerTextView.setText("00:00");
+                imageView.setImageResource(R.drawable.timer_finish_image);
             }
         }.start();
     }
@@ -44,7 +52,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button startButton = findViewById(R.id.startButton);
         timerTextView = findViewById(R.id.timerTextView);
+
+        SeekBar timeSeekBar = findViewById(R.id.timeSeekBar);
+
+        timeSeekBar.setMax(3600);
+        timeSeekBar.setProgress((int) totalTime);
+        timerTextView.setText(convertTimeText(totalTime));
+
+        timeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                Log.i("SeekBar progress", Integer.toString(progress));
+                totalTime = (Math.max(progress, 1));
+                timerTextView.setText(convertTimeText(totalTime));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+        });
+
     }
 }
